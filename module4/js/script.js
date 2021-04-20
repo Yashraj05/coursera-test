85c1b64139f2add09bf80f952bf0a6588a41f624
Yashraj <yashrajsinghmandloi@email.com>
2021-04-21 00:28:47 +0530

    module4 solution

--- module4/js/script.js
+++ module4/js/script.js
@@ -1,66 +1,16 @@
-// var x ="Hello global";
-// console.log("message is :"+x);
 
-// var a = function() {
-// 	var x ="Hello in a";
-// 	console.log("a:message is :"+x);
-// 	function p(){
-// 	console.log("p: messageis :"+x);
-// }
-// 	p();
-// }
-// a();
 
-// function Circle(radius){
-// 	this.radius = radius;
-// }
-// Circle.prototype.getArea = 
-// function() {
-// 	return Math.PI * Math.pow(this.radius,2);
-// };
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
-// var myCircle = new Circle(10);
-// console.log(myCircle.getArea());
+for (var i=0 ; i<names.length;i++) {
+		console.log(names.charAt(i));
 
-// var myOtherCircle  = new Circle(20);
-// console.log(myOtherCircle.getArea());
-function makeMultilplier(multiplier){
-	var multiplier = 3;
-	function a(){
-		console.log("multiplier is :"+multiplier);
 	}
-	a();
-
-	return(
-		function (x) {
-			return multiplier*x;
-		}
-
-	);
-}
-var douAll = makeMultilplier(4);
-console.log(douAll(10));
+	
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//   if () {
+    
+//   } else {
+//   }
+// }
